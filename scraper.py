3da7584c1d95ae9eb75e0db04df6e443ff961e49
Zarrh <fagaraz.luca@gmail.com>
2024-07-19 16:32:49 +0200

    Back to firefox

--- scraper.py
+++ scraper.py
@@ -1,14 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-# from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 
 import time
 import re
 import subprocess
-import asyncio
 
 HOST = "https://www.portaleargo.it/argoweb/famiglia/index1.jsp"
 URL = "https://www.portaleargo.it/auth/sso/login"
@@ -16,7 +13,7 @@ URL = "https://www.portaleargo.it/auth/sso/login"
 def scrap_grades(username: str, password: str, school_code: str) -> tuple[list, list]:
 
     ## For snap: ##
-    isChrome = True
+    isChrome = False
 
     if not isChrome:
         try:
@@ -50,7 +47,7 @@ def scrap_grades(username: str, password: str, school_code: str) -> tuple[list, 
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+        driver = webdriver.Chrome(options=options)
 
     driver.get(HOST)
 
